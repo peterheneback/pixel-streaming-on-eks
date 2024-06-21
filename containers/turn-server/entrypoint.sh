74ae8f0538686167fb9d90e3915ccd3235c738ee
EC2 Default User <ec2-user@ip-172-31-36-200.eu-west-1.compute.internal>
2024-06-21 14:59:23 +0000

    Fetch turn server creds from Secrets Manager

--- containers/turn-server/entrypoint.sh
+++ containers/turn-server/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Determine external ip address
 if [ -z "$EXTERNAL_IP" ]; then
-    EXTERNAL_IP=$(curl -s icanhazip.com)
+    EXTERNAL_IP=$(curl -s https://checkip.amazonaws.com/)
 fi
 
 # Get internal ip address from eth0
@@ -10,4 +10,8 @@ if [ -z "$INTERNAL_IP" ]; then
     INTERNAL_IP=$(ip addr show eth0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
 fi
 
+USER=$(aws secretsmanager get-secret-value --secret-id pixel/ice/user --query "SecretString" --output text)
+PASSWORD=$(aws secretsmanager get-secret-value --secret-id pixel/ice/password --query "SecretString" --output text)
+
+sed -e "s/USERCREDS/${USER}:${PASSWORD}/" -i /etc/turnserver.conf
 turnserver --log-file=stdout --external-ip="$EXTERNAL_IP" --listening-ip="$INTERNAL_IP" 
